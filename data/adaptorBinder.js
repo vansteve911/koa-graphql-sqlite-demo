719c2cd3ddb26678e83260b6e7441089ba7c5008
vansteve911 <vansteve911@gmail.com>
2019-03-05 23:59:12 +0800

    add support for async function; add mock db adaptor

--- data/adaptorBinder.js
+++ data/adaptorBinder.js
@@ -6,47 +6,78 @@ function checkFunctionExists(func, message) {
 }
 
 function DataAdaptorBinder() {
-  const adaptors = {} 
+  const adaptors = {}
   const self = this
-  return {
-    bind(resolver) {
-      let name = resolver.name
-      if (adaptors[name]) {
-        console.warn(`will overwrite existed resolver ${name}!`)
-      }
-      const rowToData = resolver.rowToData || (x => x)
-      const dataToRow = resolver.dataToRow || (x => x)
-      adaptors[name] = {
-        getOne: (id) => {
-          checkFunctionExists(resolver.getOne, 'not supported')
-          return rowToData(resolver.getOne(id))
-        },
-        getList: (query) => {
-          checkFunctionExists(resolver.getList, 'not supported')
-          return resolver.getList(query)
-            .then(rowToData) // TODO difference sync & async 
-        },
-        add: (data) => {
-          checkFunctionExists(resolver.add, 'not supported')
-          let newRow = dataToRow(data)
-          let result = resolver.add(newRow)
-          return Object.assign(rowToData(result), data)
-        },
-        modify: (data) => {
-          checkFunctionExists(resolver.modify, 'not supported')
-          let row = dataToRow(data)
-          return resolver.modify(row)
-        },
-        delete: (query) => {
-          checkFunctionExists(resolver.delete, 'not supported')
-          return resolver.delete(query)
-        }
-      }
-      return self
-    },
-    find(name) {
-      return adaptors[name]
+
+  function bind(resolver) {
+    let name = resolver.name
+    if (adaptors[name]) {
+      console.warn(`will overwrite existed resolver ${name}!`)
     }
+    const rowToData = resolver.rowToData || (x => x)
+    const dataToRow = resolver.dataToRow || (x => x)
+    adaptors[name] = {
+      // sync functions
+      getOne(id) {
+        checkFunctionExists(resolver.getOne, 'not supported')
+        return rowToData(resolver.getOne(id))
+      },
+      getList(query) {
+        checkFunctionExists(resolver.getList, 'not supported')
+        return resolver.getList(query)
+          .then(rowToData) // TODO difference sync & async 
+      },
+      add(data) {
+        checkFunctionExists(resolver.add, 'not supported')
+        let newRow = dataToRow(data)
+        let result = resolver.add(newRow)
+        return Object.assign(rowToData(result), data)
+      },
+      modify(data) {
+        checkFunctionExists(resolver.modify, 'not supported')
+        let row = dataToRow(data)
+        return resolver.modify(row)
+      },
+      delete(query) {
+        checkFunctionExists(resolver.delete, 'not supported')
+        return resolver.delete(query)
+      },
+      // async functions
+      getOneAsync: async function(id) {
+        checkFunctionExists(resolver.getOneAsync, 'not supported')
+        let result = await resolver.getOneAsync(id)
+        return rowToData(result)
+      },
+      getListAsync: async function(query) {
+        checkFunctionExists(resolver.getListAsync, 'not supported')
+        let result = await resolver.getListAsync(query)
+        return result.map(rowToData)
+      },
+      addAsync: async function(data) {
+        checkFunctionExists(resolver.addAsync, 'not supported')
+        let newRow = dataToRow(data)
+        return await resolver.addAsync(newRow)
+      },
+      modifyAsync: async function(data) {
+        checkFunctionExists(resolver.modifyAsync, 'not supported')
+        let row = dataToRow(data)
+        return await resolver.modifyAsync(row)
+      },
+      deleteAsync: async function(query) {
+        checkFunctionExists(resolver.deleteAsync, 'not supported')
+        return await resolver.deleteAsync(query)
+      }
+    }
+    return self
+  }
+
+  function find(name) {
+    return adaptors[name]
+  }
+
+  return {
+    bind,
+    find
   }
 }
 
--- data/mockDatabaseAdaptor.js
+++ data/mockDatabaseAdaptor.js
@@ -18,34 +18,45 @@ const mockDb = {
 
 const validKeys = new Set(['name', 'badge', 'descrition'])
 
-const GoTResolver = {
-  name: 'gameOfThrone',
-  getOne: async function (id) {
-    let result = mockDb[id]
-    return result
-  },
-  getList: async function(query) {
-    let key = Object.keys(query).find(k => validKeys.has(k))
-    if (key) {
-      let value = query[key]
-      return Object.keys(mockDb)
-        .map(id => mockDb[id])
-        .filter(record => {
-          return record[key] == value
-        })
+function LocalDataAdaptor(localData, meta) = {
+  assert(meta.name, 'name is required')
+  assert(meta.keys, 'name is required')
+  return {
+    name: 'gameOfThrone',
+      getOne(id) {
+        let result = mockDb[id]
+        return result
+      },
+      getList(query) {
+        let key = Object.keys(query).find(k => validKeys.has(k))
+        if (key) {
+          let value = query[key]
+          return Object.keys(mockDb)
+            .map(id => mockDb[id])
+            .filter(record => {
+              return record[key] == value
+            })
+        }
+        return []
+      },
+      getOneAsync: async function(id) {
+        return getOne(id)
+      },
+      getListAsync: async function(query) {
+        return getList(query)
+      }
     }
-    return []
-  }
+
 }
 
-const adaptorBinder = require('./adaptorBinder')
-adaptorBinder.bind(GoTResolver)
+// const adaptorBinder = require('./adaptorBinder')
+// adaptorBinder.bind(GoTResolver)
 
-const mockAdaptor = adaptorBinder.find('gameOfThrone')
-mockAdaptor.getOne('1')
-  .then(console.log)
-mockAdaptor.getList({badge: 'wolf'})
-  .then(console.log)
+// const mockAdaptor = adaptorBinder.find('gameOfThrone')
+// mockAdaptor.getOneAsync('1')
+//   .then(console.log)
+// mockAdaptor.getListAsync({badge: 'wolf'})
+//   .then(console.log)
 
 
-module.exports = GoTResolver
+module.exports = LocalDataAdaptor

4d26a285fe64a73ebe981d922e791fcdc30d2e29
vansteve911 <vansteve911@gmail.com>
2019-03-22 01:27:24 +0800

    file storage supports

--- storage/bindStorage.js
+++ storage/bindStorage.js
@@ -1,26 +1,26 @@
-function func(f) {
+function funcOrNone(f) {
   return (f instanceof Function) && f
 }
 
 module.exports = function(storage) {
-  const rowToData = func(storage.rowToData) || (x => x)
-  const dataToRow = func(storage.dataToRow) || (x => x)
+  const rowToData = funcOrNone(storage.rowToData) || (x => x)
+  const dataToRow = funcOrNone(storage.dataToRow) || (x => x)
   return {
-    getOne: func(storage.getOne) && (async (root, {id}) => {
+    getOne: funcOrNone(storage.getOne) && (async (root, {id}) => {
       let result = await storage.getOne(id)
       return rowToData(result)
     }),
-    getList: func(storage.getList) && (async (root, query) => {
+    getList: funcOrNone(storage.getList) && (async (root, query) => {
       let result = await storage.getList(query)
       return result.map(rowToData)
     }),
-    add: func(storage.add) && (async (root, data) => {
+    add: funcOrNone(storage.add) && (async (root, data) => {
       return await storage.add(dataToRow(data))
     }),
-    modify: func(storage.modify) && (async (root, data) => {
+    modify: funcOrNone(storage.modify) && (async (root, data) => {
       return await storage.modify(dataToRow(data))
     }),
-    delete: func(storage.delete) && (async (root, query) => {
+    delete: funcOrNone(storage.delete) && (async (root, query) => {
       return await storage.delete(query)
     })
   }
---
+++ storage/fileStorage.js
@@ -0,0 +1,80 @@
+const assert = require('assert')
+const fs = require('fs')
+const util = require('util')
+
+const statAsync = util.promisify(fs.stat)
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+
+const loadFile = async (file, deserializer) => {
+  let content = await readFile(file, 'utf8')
+  return deserializer(content)
+}
+
+const updateFile = async (file, data, serializer) => {
+  let content = serializer(data)
+  await writeFile(file, content)
+  return data
+}
+
+const SingleDataFileStorage = function(file, {serializer, deserializer}) {
+  assert(serializer instanceof Function, 'must provide serializer')
+  assert(deserializer instanceof Function, 'must provide deserializer')
+
+  return {
+    getOne: async() => {
+      return loadFile(file, deserializer)
+    },
+    add: async(data) => {
+      return await updateFile(file, data, serializer)
+    },
+    modify: async({updates}) => {
+      if (!updates) {
+        throw Error('nothing to be updated')
+      }
+      let data = await loadFile(file, deserializer)
+      Object.assign(data, updates)
+      return await updateFile(file, data, serializer)
+    }
+  }
+}
+
+const MultiDataFileStorage = function(file, {serializer, deserializer}) {
+  // return {
+  //   getOne:
+  // }
+  // TODO 
+}
+
+// test codes
+let storage = SingleDataFileStorage('/tmp/test911.json', {
+  serializer: JSON.stringify,
+  deserializer: JSON.parse
+})
+storage.add({day: 'Easter Day', month: 'April'})
+  .then((data) => {
+    console.log('after add', data)
+    return storage.modify({
+      updates: {
+        description: 'the day when Jesus came back to life'
+      }
+    })
+  })
+  .then((data) => {
+    console.log('after modify', data)
+    return storage.getOne()
+  })
+  .then((data) => {
+    console.log('file content', data)
+  })
+  .catch((err) => {
+    console.error('something\' wrong you little fool: ', err)
+  })
+
+
+
+
+
+
+
+
